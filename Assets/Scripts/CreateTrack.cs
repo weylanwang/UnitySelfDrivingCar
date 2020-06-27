d2dba7eaf5157bf64d90cb9ce4adf692305b6edc
Daniel Park <luckynumberseven07@gmail.com>
2020-06-27 15:33:17 -0700

    Created linear, angled, and curved track pieces

---
+++ Assets/Scripts/CreateTrack.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTrack : MonoBehaviour
+{
+    public GameObject[] trackPieces;
+
+    public uint size;
+}
