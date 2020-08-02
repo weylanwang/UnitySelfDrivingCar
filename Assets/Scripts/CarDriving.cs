74594352d68c41d42512ca00353a512959cd6665
Weylan Wang <weylan.wang@berkeley.edu>
2020-08-01 17:59:07 -0700

    Updated Car
    Changelog:
    -Updated car prefab to have multiple sensors
    -Updated car sensor to have a distance value
    -Updated car driving scipt to be able to return sensor readings
    -Added an OnCollision to detect car hitting wall, will also set off an
    event
    
    Testing:
    -Go into TrackCreate and drive the car around and you should see
    multiple sensors, and if you want to debug log, you can check the
    distance values

--- Assets/Scripts/CarDriving.cs
+++ Assets/Scripts/CarDriving.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CarDriving : MonoBehaviour
 {
     #region Variables
+    #region Serialized Variables
     [SerializeField]
     [Tooltip("How fast the car can accelerate, as well as its max speed")]
     private float enginePower;
@@ -13,29 +14,59 @@ public class CarDriving : MonoBehaviour
     [Tooltip("Car's turning/handling power")]
     private float turningPower;
 
+    [SerializeField]
+    [Tooltip("The parent game object holding all the sensors")]
+    private GameObject sensorParent;
+
+    [SerializeField]
+    [Tooltip("Is this car AI controlled?")]
+    private bool AIControlled;
+
+    [SerializeField]
+    [Tooltip("The number of sensors that this car prefab has")]
+    private int numSensors;
+    #endregion
+
+    #region Public Variables
+    public delegate void WallCollision();
+    public static event WallCollision WallCollisionEvent;
+    #endregion
+
+    #region Private variables
     private float horizontalAxis;
     private float verticalAxis;
     private float velocity;
     private float rotation;
     private Rigidbody2D carRB;
+    private CarSensors[] sensors;
+    #endregion
     #endregion
 
     #region Start/Update
     private void Start()
     {
+        AIControlled = false;
         velocity = 0;
         carRB = GetComponent<Rigidbody2D>();
+
+        sensors = new CarSensors[numSensors];
+        for (int i = 0; i < numSensors; i++) {
+            sensors[i] = sensorParent.transform.GetChild(i).GetComponent<CarSensors>();
+        }
     }
+
     void FixedUpdate()
     {
-        horizontalAxis = Input.GetAxis("Horizontal");
-        verticalAxis = Input.GetAxis("Vertical");
-        ApplyManualControls();
+        if (!AIControlled) {
+            horizontalAxis = Input.GetAxis("Horizontal");
+            verticalAxis = Input.GetAxis("Vertical");
+        }
+        ApplyControls();
     }
     #endregion
 
     #region Car Control Functions
-    private void ApplyManualControls() {
+    private void ApplyControls() {
         velocity = verticalAxis * enginePower;
         rotation = Mathf.Sign(Vector2.Dot(carRB.velocity, carRB.GetRelativeVector(Vector2.up)));
 
@@ -45,6 +76,24 @@ public class CarDriving : MonoBehaviour
 
         //Small drift effect to make the car feel more realistic
         carRB.AddRelativeForce(-Vector2.right * carRB.velocity.magnitude * (-horizontalAxis / 2));
+    }
+
+    //Values should be between -1 and 1
+    public void SetCarInputs(float[] inputs) {
+        horizontalAxis = inputs[0];
+        verticalAxis = inputs[1];
+    }
+
+    public float[] GetSensorReadings() {
+        float[] sensorReadings = new float[numSensors];
+        for (int i = 0; i < numSensors; i++) {
+            sensorReadings[i] = sensors[i].GetSensorReading();
+        }
+        return sensorReadings;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        WallCollisionEvent();
     }
     #endregion
 }
